d39ee4ac6723e7f08e0ba8e6b1b5869fbc62860e
Gaetan Semet <gaetan@xeberon.net>
2017-01-19 09:18:53 +0100

    Restrict to python 3
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 import setuptools
+import sys
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only Python 3 is supported.")
 
 setuptools.setup(
     setup_requires=['pbr'],
